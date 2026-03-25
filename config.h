c174473f8fb72c054a5b57b336f481d6ef1f61d6
veer g <161938487+veersatyag@users.noreply.github.com>
2026-03-25 21:47:50 +0530

    Create config.h

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ------------------- WIFI -------------------
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+// ------------------- MQTT -------------------
+#define MQTT_SERVER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "water/esp32/basic"
+
+#endif
